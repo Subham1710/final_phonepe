ead74f7fddd8a1a8300237e097614c1b4f6f0998
Bhadkaashiq <subhammishra@MacBook-Air-5.local>
2023-08-30 15:57:04 +0530

    added quarter

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,211 @@
-import React from 'react';
+// import React from 'react';
+// import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+// import { Pie } from 'react-chartjs-2';
+// import { Bar } from 'react-chartjs-2';
+// import DATA from './hierarchical_combined.json'
+// import './App.css';
+// import Dropdown from './Dropdown';
+// import { useState } from 'react';
+
+// ChartJS.register(ArcElement, Tooltip, Legend);
+
+// const styles = {
+//   container: {
+//     display: 'flex',
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     gap: '40px',
+//   },
+//   chartContainer: {
+//     display: 'flex',
+//     flexDirection: 'column',
+//     alignItems: 'center',
+//     gap: '20px',
+//   },
+//   header: {
+//     backgroundColor: '#333',  // dark background
+//     color: '#fff',  // white text
+//     padding: '10px 0',
+//     textAlign: 'center',
+//     fontSize: '24px',
+//     marginBottom: '40px',  // space between the header and the charts
+//     fontFamily: 'Arial, sans-serif',
+//     boxShadow: '0px 3px 10px rgba(0, 0, 0, 0.2)'  // subtle shadow for depth
+//   }
+// };
+
+// const PieChartComponent = ({ title, data }) => (
+//   <div style={styles.chartContainer}>
+//     <h2>{title}</h2>
+//     <Pie data={data} />
+//   </div>
+// );
+
+
+// const App = () => {
+//   //show data on console
+//   const yearList = [2018, 2019, 2020, 2021, 2022];
+//   const [currentYear, setCurrentYear] = useState(2020);
+//   const [currentQuarter, setCurrentQuarter] = useState(0);
+//   const dataMap = {};
+//   const finaldata= {};
+
+//   yearList.forEach(year => {
+//     if (year === currentYear){
+//     for (let i=0; i<4; i++) {
+//     if (i === currentQuarter ){
+
+//       let key = year + '_' + 'Q' +  (i+1); 
+//       let currentFileName = (i + 1) + '.json';
+//       dataMap[key] = DATA.transaction.hover.country.india[year][currentFileName];
+//       finaldata = dataMap[key]; 
+//       console.log(key, currentFileName, dataMap[key]);
+//     }
+//   }
+//   }
+//   });  
+//   let pcd = {
+//     labels: 'a',
+//     datasets: [
+//       {
+//         label: '# of Types',
+//         data: finaldata,
+//         backgroundColor: [
+//           'rgba(255, 99, 132, 0.2)',
+//           'rgba(54, 162, 235, 0.2)',
+//           'rgba(255, 206, 86, 0.2)',
+//           'rgba(75, 192, 192, 0.2)',
+//           'rgba(153, 102, 255, 0.2)',
+//           'rgba(255, 159, 64, 0.2)',
+//         ],
+//         borderColor: [
+//           'rgba(255, 99, 132, 1)',
+//           'rgba(54, 162, 235, 1)',
+//           'rgba(255, 206, 86, 1)',
+//           'rgba(75, 192, 192, 1)',
+//           'rgba(153, 102, 255, 1)',
+//           'rgba(255, 159, 64, 1)',
+//         ],
+//         borderWidth: 1,
+//       },
+//     ],
+//   };
+
+//   let labels = [];
+//   let pieChartData = [];
+
+//   console.log(dataMap);
+//   Object.values(dataMap).forEach(currentData => {
+//     let currentLabels = [];
+//     let currentPieChartData = [];
+
+//     currentData.data.hoverDataList.forEach(stateData => {
+//       currentLabels.push(stateData.name);
+//       currentPieChartData.push(stateData.metric[0].count);
+//     })
+
+//     labels.push(currentLabels);
+//     pieChartData.push(currentPieChartData);
+//   });
+
+
+//   console.log(labels, pieChartData);
+//   const totalPieChartsData = [];
+//   for (let i=0; i<labels.length; i++) {
+//     let data = {
+//       labels: labels[i],
+//       datasets: [
+//         {
+//           label: '# of Types',
+//           data: pieChartData[i],
+//           backgroundColor: [
+//             'rgba(255, 99, 132, 0.2)',
+//             'rgba(54, 162, 235, 0.2)',
+//             'rgba(255, 206, 86, 0.2)',
+//             'rgba(75, 192, 192, 0.2)',
+//             'rgba(153, 102, 255, 0.2)',
+//             'rgba(255, 159, 64, 0.2)',
+//           ],
+//           borderColor: [
+//             'rgba(255, 99, 132, 1)',
+//             'rgba(54, 162, 235, 1)',
+//             'rgba(255, 206, 86, 1)',
+//             'rgba(75, 192, 192, 1)',
+//             'rgba(153, 102, 255, 1)',
+//             'rgba(255, 159, 64, 1)',
+//           ],
+//           borderWidth: 1,
+//         },
+//       ],
+//     };
+//     totalPieChartsData.push(data);
+//   }
+ 
+//   console.log(currentYear);
+
+//   return (
+//     <div style={{ padding: '20px' }}>
+//       <div style={styles.header}>PhonePe Data Dashboard</div>
+//       <Dropdown yearList={yearList} currentYear={currentYear} setCurrentYear={setCurrentYear} />
+//       <div style={styles.container}>
+
+//        <div> <PieChartComponent title="Region Data" data={pcd}/> </div>
+
+      
+//       </div>
+      
+//     </div>
+//   );
+  
+// };
+        
+
+
+
+
+// ;
+ 
+
+
+// export default App;
+
+
+import React, { useState, useEffect } from 'react';
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 import { Pie } from 'react-chartjs-2';
-import { Bar } from 'react-chartjs-2';
-import DATA from './hierarchical_combined.json'
+import DATA from './hierarchical_combined.json';
 import './App.css';
 import Dropdown from './Dropdown';
-import { useState } from 'react';
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 const styles = {
   container: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    gap: '40px',
-  },
-  chartContainer: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    gap: '20px',
-  },
-  header: {
-    backgroundColor: '#333',  // dark background
-    color: '#fff',  // white text
-    padding: '10px 0',
-    textAlign: 'center',
-    fontSize: '24px',
-    marginBottom: '40px',  // space between the header and the charts
-    fontFamily: 'Arial, sans-serif',
-    boxShadow: '0px 3px 10px rgba(0, 0, 0, 0.2)'  // subtle shadow for depth
-  }
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        gap: '40px',
+      },
+      chartContainer: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        gap: '20px',
+      },
+      header: {
+        backgroundColor: '#333',  // dark background
+        color: '#fff',  // white text
+        padding: '10px 0',
+        textAlign: 'center',
+        fontSize: '24px',
+        marginBottom: '40px',  // space between the header and the charts
+        fontFamily: 'Arial, sans-serif',
+        boxShadow: '0px 3px 10px rgba(0, 0, 0, 0.2)'  // subtle shadow for depth
+    
+  // ... (same as before)
+      }
 };
+
 
 const PieChartComponent = ({ title, data }) => (
   <div style={styles.chartContainer}>
@@ -41,35 +214,35 @@ const PieChartComponent = ({ title, data }) => (
   </div>
 );
 
-
 const App = () => {
-  //show data on console
   const yearList = [2018, 2019, 2020, 2021, 2022];
   const [currentYear, setCurrentYear] = useState(2020);
   const [currentQuarter, setCurrentQuarter] = useState(0);
-  const dataMap = {};
-  const finaldata= {};
+  const [finalData, setFinalData] = useState(null);
 
-  yearList.forEach(year => {
-    if (year === currentYear){
-    for (let i=0; i<4; i++) {
-    if (i === currentQuarter ){
+  useEffect(() => {
+    const key = `${currentYear}_Q${currentQuarter + 1}`;
+    const currentFileName = `${currentQuarter + 1}.json`;
+    const data = DATA.transaction.hover.country.india[currentYear][currentFileName];
+    setFinalData(data);
+  }, [currentYear, currentQuarter]);
 
-      let key = year + '_' + 'Q' +  (i+1); 
-      let currentFileName = (i + 1) + '.json';
-      dataMap[key] = DATA.transaction.hover.country.india[year][currentFileName];
-      finaldata = dataMap[key]; 
-      console.log(key, currentFileName, dataMap[key]);
-    }
+  let labels = [];
+  let pieChartData = [];
+
+  if (finalData && finalData.data && finalData.data.hoverDataList) {
+    finalData.data.hoverDataList.forEach(stateData => {
+      labels.push(stateData.name);
+      pieChartData.push(stateData.metric[0].count);
+    });
   }
-  }
-  });  
-  let pcd = {
-    labels: 'a',
+
+  const chartData = {
+    labels: labels,
     datasets: [
       {
         label: '# of Types',
-        data: finaldata,
+        data: pieChartData,
         backgroundColor: [
           'rgba(255, 99, 132, 0.2)',
           'rgba(54, 162, 235, 0.2)',
@@ -91,85 +264,17 @@ const App = () => {
     ],
   };
 
-  let labels = [];
-  let pieChartData = [];
-
-  console.log(dataMap);
-  Object.values(dataMap).forEach(currentData => {
-    let currentLabels = [];
-    let currentPieChartData = [];
-
-    currentData.data.hoverDataList.forEach(stateData => {
-      currentLabels.push(stateData.name);
-      currentPieChartData.push(stateData.metric[0].count);
-    })
-
-    labels.push(currentLabels);
-    pieChartData.push(currentPieChartData);
-  });
-
-
-  console.log(labels, pieChartData);
-  const totalPieChartsData = [];
-  for (let i=0; i<labels.length; i++) {
-    let data = {
-      labels: labels[i],
-      datasets: [
-        {
-          label: '# of Types',
-          data: pieChartData[i],
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)',
-          ],
-          borderColor: [
-            'rgba(255, 99, 132, 1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)',
-          ],
-          borderWidth: 1,
-        },
-      ],
-    };
-    totalPieChartsData.push(data);
-  }
- 
-  console.log(currentYear);
-
   return (
     <div style={{ padding: '20px' }}>
       <div style={styles.header}>PhonePe Data Dashboard</div>
       <Dropdown yearList={yearList} currentYear={currentYear} setCurrentYear={setCurrentYear} />
       <div style={styles.container}>
-
-       <div> <PieChartComponent title="Region Data" data={pcd}/> </div>
-
-      
+        <PieChartComponent title="Region Data" data={chartData} />
       </div>
-      
     </div>
   );
-  
 };
-        
-
-
-
-
-;
- 
-
 
 export default App;
-
-
-
 
 
--- src/dropdown.jsx
+++ src/dropdown.jsx
@@ -1,24 +1,37 @@
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 
-function Dropdown({ yearList, currentYear, setCurrentYear}) {
+function Dropdown({ yearList, currentYear, setCurrentYear }) {
   const [isOpen, setIsOpen] = useState(false);
-    
-//const yearListItems = yearList.map(year => <li onClick={() => setCurrentYear(year)}>{year}</li>);
-const yearListItems = yearList.map(year => <li key={year} onClick={() => setCurrentYear(year)}>{year}</li>);
 
-  console.log(yearList); 
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const yearListItems = yearList.map(year => (
+    <li key={year} onClick={() => setCurrentYear(year)}>
+      {year}
+    </li>
+  ));
+
   return (
     <div className="dropdown">
-      <button onClick={() => setIsOpen(!isOpen)}>
+      <button onClick={toggleDropdown} aria-haspopup="true" aria-expanded={isOpen}>
         Years
       </button>
       {isOpen && (
-        <ul >
-            {yearListItems}
+        <ul aria-label="Year List">
+          {yearListItems}
         </ul>
       )}
     </div>
   );
 }
+
+Dropdown.propTypes = {
+  yearList: PropTypes.array.isRequired,
+  currentYear: PropTypes.number.isRequired,
+  setCurrentYear: PropTypes.func.isRequired,
+};
 
 export default Dropdown;
